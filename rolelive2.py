fa50b718caebc95c8b80f83e47acf593eba9e8fd
Marc Sances <marcsances@gmail.com>
2020-10-15 21:32:14 +0200

    Update rolelive2.py

--- rolelive2.py
+++ rolelive2.py
@@ -1,23 +1,22 @@
-import requests
-import sys
-import time
-import discord
-import datetime
-from discord import Streaming
-import uuid
-from threading import Timer
-import json
 import asyncio
+import json
+import uuid
 
-GUILD_ID=GUILD_ID
-ROLE_IDS=[ROLE_IDS]
-ADMIN_IDS=[ADMIN_IDS]
-ADMIN_USERS=[ADMIN_USERS]
-CLIENT_SECRET='CLIENT_SECRET'
-CHANNEL_ID=CHANNEL_ID
-MESSAGE="Hey guys! %s is live on %s! Check it out at %s"
-TWITCH_CLIENT_ID="TWITCH_ID"
-INTERVAL=30
+import discord
+import requests
+
+print("Please set all the IDs and secrets before attempting to use this bot!. Remove this print after you've done it.")
+TWITCH_CLIENT_ID="SET_THIS_PLEASE"
+INTERVAL=120
+ROLE_NAMES=["DJ", "Producer"]
+MESSAGE="%s is now live: **%s** - _%s_ - %s"
+VERSION="0.3.0"
+GUILD_ID=SET_THIS_PLEASE
+ROLE_IDS=[SET, THIS, PLEASE]
+ADMIN_IDS=[SET_THIS_PLEASE]
+ADMIN_USERS=[SET_THIS_PLEASE]
+CLIENT_SECRET='SET_THIS_PLEASE'
+CHANNEL_ID=SET_THIS_PLEASE
 
 client = discord.Client()
 
@@ -34,19 +33,22 @@ def check_on_twitch(member):
             if response2.status_code == 200:
                 body2 = response2.json()
                 if body2["stream"] != None:
-                    return True
-    return False
+                    return body2
+    return None
 
 async def perform_check():
     global members_online
     while True:
         found = []
         for stream in set(all_members.values()):
-            if check_on_twitch(stream):
+            stream_info = check_on_twitch(stream)
+            if stream_info is not None:
                 if stream not in members_online:
                     members_online.append(stream)
                     channel = client.get_channel(CHANNEL_ID)
-                    await channel.send(MESSAGE % (stream, "Twitch", "https://twitch.tv/" + stream))
+                    title = stream_info["stream"]["channel"]["status"]
+                    game = stream_info["stream"]["channel"]["game"]
+                    await channel.send(MESSAGE % (stream, title, game, "https://twitch.tv/" + stream))
                 found.append(stream)
             await asyncio.sleep(2)
         for stream in members_online:
@@ -80,32 +82,27 @@ async def on_message(message):
     channel = message.channel
     if message.content.startswith("!addstream"):
         if len(message.content) < 14:
-            await channel.send(message.author.name + ", please tell me which is your stream! !addstream your_stream_name")
+            await channel.send(message.author.name + ", please tell me which is your stream! !addstream your_stream_name\nType !rolelive for help.")
             return
         if "twitch.tv/" in message.content:
-            await channel.send("Sorry " + message.author.name + ", you should add your stream name without the twitch.tv/ URL.")
+            await channel.send("Sorry " + message.author.name + ", you should add your stream name without the twitch.tv/ URL.\nType !rolelive for help.")
             return
         if not has_role(message.author):
-            await channel.send("Sorry " + message.author.name + ", your role is not authorized to add streams to the live channel. You must be of either DJ or Producer role.")
+            await channel.send("Sorry " + message.author.name +
+                               ", your role is not authorized to add streams to the live channel. You must have one of these roles: {roles}\nType !rolelive for help."
+                               .format(roles=", ".join(ROLE_NAMES)))
             return
-        all_members[message.author.id] = message.content.replace("!addstream ", "")
-        with open("members.json", "w") as f:
-            json.dump(all_members, f)
-        await channel.send("OK " + message.author.name + ", I added your stream to the list")
+        await add_stream_of_user(all_members, channel, message)
     elif message.content.startswith("!removestream"):
         if message.author.id not in all_members:
-            await channel.send("Sorry " + message.author.name + ", you don't have any stream configured in this bot")
+            await channel.send("Sorry " + message.author.name + ", you don't have any stream configured in this bot\nType !rolelive for help.")
             return
         else:
-            del all_members[message.author.id]
-            with open("members.json", "w") as f:
-                json.dump(all_members, f)
-            await channel.send("OK " + message.author.name + ", I'll no longer track your stream.")
+            await remove_stream_of_user(all_members, channel, message)
     elif message.content.startswith("!onlinestreams"):
-        msg = "Streams currently online:\n"
-        for stream in members_online:
-            msg = msg + stream + " - <https://twitch.tv/" + stream + ">\n"
-        await channel.send(msg)
+        await show_streams_online(channel, members_online)
+    elif message.content.startswith("!streaminfo"):
+        await show_stream_info(channel, message)
     elif message.content.startswith("!admin"):
         if not is_admin(message.author):
             await channel.send("Sorry " + message.author.name + ", you must be administrator to manage streamer list")
@@ -115,34 +112,111 @@ async def on_message(message):
             if "twitch.tv" in message.content:
                 await channel.send("Sorry " + message.author.name + ", you should add your stream name without the twitch.tv/ URL.")
                 return
-            all_members[str(uuid.uuid4())] = cmd.replace("add ", "")
-            with open("members.json", "w") as f:
-                json.dump(all_members, f)
-            await channel.send("OK " + message.author.name + ", I added that stream to the list")
+            await admin_add(all_members, channel, cmd, message)
         elif cmd.startswith("remove"):
-            target = cmd.replace("remove ", "")
-            keys = map(lambda x: x[0], filter(lambda x: x[1] == target, all_members.items()))
-            for key in list(keys)[:]:
-                del all_members[key]
-            with open("members.json", "w") as f:
-                json.dump(all_members, f)
-            await channel.send("OK " + message.author.name + ", I've removed that stream from the list")
+            await admin_remove(all_members, channel, cmd, message)
         elif cmd.startswith("list"):
             await channel.send("This are all the streams I know: " + ", ".join(all_members.values()))
         elif cmd.startswith("help"):
             await channel.send("Admin help:\nAdd a stream with !admin.add stream_name\nRemove a stream with !admin.remove stream_name\nList all streams with !admin.list.")
         elif cmd.startswith("reload"):
-            try:
-                with open("members.json", "r") as f:
-                    all_members = json.load(f)
-            except:
-                all_members = dict()
-            try:
-                with open("alive.json", "r") as f:
-                    members_online = json.load(f)
-            except:
-                members_online = list()
-            await channel.send("Reloaded streamer list")
+            await reload_list(channel)
+    elif message.content.startswith("!rolelive"):
+        await rolelive_help(channel)
+
+
+async def rolelive_help(channel):
+    message = "**RoleLive Help** (RoleLive version " + VERSION + " by Marquii)\n```"
+    message = message + "This bot keeps track of Twitch streams and notifies in channel #" + channel.name + " when users go live.\n"
+    message = message + "To add yourself to this bot, make sure you have one of the following roles: " + ", ".join(ROLE_NAMES) + "\n"
+    message = message + "Then type '!addstream stream_name'. Do not type the twitch.tv side of the URL, just the stream name.\n"
+    message = message + "For example, if your channel is <twitch.tv/FooBar>, type '!addstream FooBar'.\n"
+    message = message + "If you wanna update your stream name, just type !addstream again.\n"
+    message = message + "To remove your stream from the bot, type !removestream.\n"
+    message = message + "You can check online channels by typing !onlinestreams.\n"
+    message = message + "If you're a server administrator, type !admin.help for a list of admin commands.\n"
+    message = message + "```\nPsst! Want this in your server? Visit <https://github.com/marcsances/rolelive>."
+    await channel.send(message)
+
+async def add_stream_of_user(all_members, channel, message):
+    all_members[message.author.id] = message.content.replace("!addstream ", "")
+    with open("members.json", "w") as f:
+        json.dump(all_members, f)
+    await channel.send("OK " + message.author.name + ", I added your stream to the list")
+    await update_status()
+
+
+async def remove_stream_of_user(all_members, channel, message):
+    del all_members[message.author.id]
+    with open("members.json", "w") as f:
+        json.dump(all_members, f)
+    await channel.send("OK " + message.author.name + ", I'll no longer track your stream.")
+    await update_status()
+
+
+async def show_streams_online(channel, members_online):
+    msg = "Streams currently online:\n"
+    for stream in members_online:
+        msg = msg + stream + " - <https://twitch.tv/" + stream + ">\n"
+    msg = msg + "Type !streaminfo <stream_name> to get more information for a certain stream"
+    await channel.send(msg)
+
+
+async def show_stream_info(channel, message):
+    user = message.content.replace("!streaminfo ", "")
+    stream_info = check_on_twitch(user)
+    if stream_info is not None:
+        game = stream_info["stream"]["channel"]["game"]
+        title = stream_info["stream"]["channel"]["status"]
+        viewers = stream_info["stream"]["viewers"]
+        link = "https://twitch.tv/" + user
+        message = "{user} is live playing {game}\n{title}\n{viewers} viewers\nWatch on {link}".format(user=user,
+                                                                                                      game=game,
+                                                                                                      title=title,
+                                                                                                      viewers=viewers,
+                                                                                                      link=link)
+        await channel.send(message)
+    else:
+        await channel.send("This stream seems to be offline or may not exist.")
+
+
+async def admin_add(all_members, channel, cmd, message):
+    all_members[str(uuid.uuid4())] = cmd.replace("add ", "")
+    with open("members.json", "w") as f:
+        json.dump(all_members, f)
+    await channel.send("OK " + message.author.name + ", I added that stream to the list")
+    await update_status()
+
+
+async def admin_remove(all_members, channel, cmd, message):
+    target = cmd.replace("remove ", "")
+    keys = map(lambda x: x[0], filter(lambda x: x[1] == target, all_members.items()))
+    for key in list(keys)[:]:
+        del all_members[key]
+    with open("members.json", "w") as f:
+        json.dump(all_members, f)
+    await channel.send("OK " + message.author.name + ", I've removed that stream from the list")
+    await update_status()
+
+
+async def reload_list(channel):
+    global all_members, members_online
+    try:
+        with open("members.json", "r") as f:
+            all_members = json.load(f)
+    except:
+        all_members = dict()
+    try:
+        with open("alive.json", "r") as f:
+            members_online = json.load(f)
+    except:
+        members_online = list()
+    await channel.send("Reloaded streamer list")
+    await update_status()
+
+
+async def update_status():
+    await client.change_presence(activity=discord.Game("!rolelive - Watching " + str(len(all_members)) + " streams"))
 
 
 @client.event
@@ -160,9 +234,8 @@ async def on_ready():
     except:
         members_online = list()
     client.loop.create_task(perform_check())
+    await update_status()
     print("Ready")
 
 
 client.run(CLIENT_SECRET)
-
-# bot invite url: https://discord.com/oauth2/authorize?client_id=736694316059328594&scope=bot&permissions=3072
